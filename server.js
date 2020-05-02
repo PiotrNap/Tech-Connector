dd2e05e096729d47df9db6e1f447d0deebdfddca
PiotrNap <piotr.napierala94@gmail.com>
2020-05-02 17:53:10 +0200

    initial commmit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("api running");
+});
+
+app.listen(PORT, () => console.log("Listening on port 5000"));
